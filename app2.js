91925bbf7d5e689cdcf3d5b487ae72ad38fb76e2
Allison Livingston <livingstona16@gmail.com>
2018-04-27 14:19:13 -0600

    Adding updated JS files

--- app2.js
+++ app2.js
@@ -1,6 +1,8 @@
 const baseUrl = 'https://swapi.co/api/'
 const characterSection = document.querySelector('.characters')
-const charPics = ['./Star-Wars-Pics/luke.jpg', './Star-Wars-Pics/C3PO.jpg', './Star-Wars-Pics/R2D2.jpg', './Star-Wars-Pics/darth-vader.jpg', './Star-Wars-Pics/leia.jpg', './Star-Wars-Pics/Owen-OP.jpg', './Star-Wars-Pics/Biggs.png', './Star-Wars-Pics/Obiwankenobi.jpg', './Star-Wars-Pics/Beru.png', './Star-Wars-Pics/r5-d4.jpg']
+const charPics = ['./Star-Wars-Pics/luke.jpg', './Star-Wars-Pics/C3PO.jpg', './Star-Wars-Pics/R2D2.jpg', './Star-Wars-Pics/darth-vader.jpg', './Star-Wars-Pics/leia.jpg', './Star-Wars-Pics/Owen-OP.jpg', './Star-Wars-Pics/Beru.png', './Star-Wars-Pics/r5-d4.jpg', './Star-Wars-Pics/Biggs.png', './Star-Wars-Pics/Obiwankenobi.jpg']
+const pickUpLines = ["Unlike Han, I won't shoot first", "Nice buns, Princess", "You're the Obi-Wan for me", "You've been looking for love in Alderaan places", "Size matters not", "Damn girl you R2 fine, if you're lucky I'll give your the D2", "Let's get Hoth and heavy", "Jedi in the Streets, Sith in the Sheets", "R4 is red, R2 is blue. If I was the force, I'd be in you."]
+
 
 function onClick(event) {
   event.preventDefault()
@@ -41,6 +43,7 @@ fetch(baseUrl + 'people')
             <li>Eye Color: ${eyes}</li>
             <li>Birth Year: ${birthYear}</li>
             <li>Gender: ${gender}</li>
+            <li>Pickup Line: "${pickUpLines[i]}"
           </ul>
           <div>
             <button>Nope</button>
@@ -53,8 +56,11 @@ fetch(baseUrl + 'people')
     let selectButtons = document.querySelectorAll('button')
     let images = document.querySelectorAll('img')
 
-    for(let i=0; i<selectButtons.length; i++) {
+    for(let i=0; i<10; i++) {
       images[i].src = charPics[i]
+      selectButtons[i].addEventListener('click', onClick)
+    }
+    for(let i=10; i<selectButtons.length; i++) {
       selectButtons[i].addEventListener('click', onClick)
     }
   })
